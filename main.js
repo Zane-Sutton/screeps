97a6973f32211b8a506f3ae1a738d910276da585
Zane Sutton <zane@factoryx.lan>
2017-11-03 17:10:34 +1100

    app/code/core/Mage/Checkout/Helper/Data.php

--- main.js
+++ main.js
@@ -7,53 +7,67 @@ var role_Harvester = require('role_Harvester');
 var max_Harverster_Population = 10;
 
 //Combat Creeps
-//var role_Brute = require('role_Brute.js'); //Melee
-//var role_Fletcher = require('role_Fletcher.js'); //Ranged
-//var role_Mystic = require('role_Mystic.js'); //Healer
-//var role_Exarch = require('role_Exarch.js'); //Elite
+var role_Vidar = require('role_Vidar.js'); //Melee
+var role_Artemis = require('role_Artemis.js'); //Ranged
+var role_Mystic = require('role_Mystic.js'); //Healer
+var role_Exarch = require('role_Exarch.js'); //Elite
+
+
+var sources = [];
+var nodes = [];
+
+sources = Game.spawns['Nexus'].room.find(FIND_SOURCES);
+
+for(var source in sources) {
+    nodes[source] = [];
+    nodes[source][0] = sources[source]; //id
+    nodes[source][1] = sources[source].pos.x; //x position
+    nodes[source][2] = sources[source].pos.y; //x position
+    nodes[source][3] = []; //assigned harvesters
+    nodes[source][4] = 0; //max harvesters
+
+    for(var name in Game.creeps)
+    {
+      var creep = Game.creeps[name];
+      if(Game.getObjectById(creep.memory.assignednode["id"]) == nodes[source][0])
+      {
+        nodes[source][3].push(creep.name);
+      }
+    }
+
+    var terrain = [];
+    terrain[0] = Game.map.getTerrainAt(nodes[source][1],(nodes[source][2]+1),Game.spawns['Nexus'].room.name); //n
+    terrain[1] = Game.map.getTerrainAt(nodes[source][1],(nodes[source][2]-1),Game.spawns['Nexus'].room.name); //s
+    terrain[2] = Game.map.getTerrainAt(nodes[source][1]+1,(nodes[source][2]),Game.spawns['Nexus'].room.name); //e
+    terrain[3] = Game.map.getTerrainAt(nodes[source][1]-1,(nodes[source][2]),Game.spawns['Nexus'].room.name); //w
+    terrain[4] = Game.map.getTerrainAt(nodes[source][1]+1,(nodes[source][2]+1),Game.spawns['Nexus'].room.name); //ne
+    terrain[5] = Game.map.getTerrainAt(nodes[source][1]+1,(nodes[source][2]-1),Game.spawns['Nexus'].room.name); //se
+    terrain[6] = Game.map.getTerrainAt(nodes[source][1]-1,(nodes[source][2]+1),Game.spawns['Nexus'].room.name); //nw
+    terrain[7] = Game.map.getTerrainAt(nodes[source][1]-1,(nodes[source][2]-1),Game.spawns['Nexus'].room.name); //sw
+
+    for(var tile in terrain) {
+      if(terrain[tile]!="wall")
+      {
+          nodes[source][4]++;
+      }
+    }
+}
 
 module.exports.loop = function () {
   var creeps = [];
   var engineers = [];
   var enhancers = [];
   var harvesters = [];
-  var sources = [];
-  var nodes = [];
+
 
 
   creeps = _.filter(Game.creeps);
   engineers = _.filter(Game.creeps, {memory:  {role: 'engineer'}});
   enhancers = _.filter(Game.creeps, {memory:  {role: 'enhancer'}});
   harvesters = _.filter(Game.creeps, {memory:  {role: 'harvester'}});
-  sources = Game.spawns['Nexus'].room.find(FIND_SOURCES_ACTIVE);
 
-  for(var source in sources) {
-      nodes[source] = [];
-      nodes[source][0] = sources[source]; //id
-      nodes[source][1] = sources[source].pos.x; //x position
-      nodes[source][2] = sources[source].pos.y; //x position
-      nodes[source][3] = []; //assigned harvesters
-      nodes[source][4] = 0; //max harvesters
 
-      var terrain = [];
-      terrain[0] = Game.map.getTerrainAt(nodes[source][1],(nodes[source][2]+1),Game.spawns['Nexus'].room.name); //n
-      terrain[1] = Game.map.getTerrainAt(nodes[source][1],(nodes[source][2]-1),Game.spawns['Nexus'].room.name); //s
-      terrain[2] = Game.map.getTerrainAt(nodes[source][1]+1,(nodes[source][2]),Game.spawns['Nexus'].room.name); //e
-      terrain[3] = Game.map.getTerrainAt(nodes[source][1]-1,(nodes[source][2]),Game.spawns['Nexus'].room.name); //w
-      terrain[4] = Game.map.getTerrainAt(nodes[source][1]+1,(nodes[source][2]+1),Game.spawns['Nexus'].room.name); //ne
-      terrain[5] = Game.map.getTerrainAt(nodes[source][1]+1,(nodes[source][2]-1),Game.spawns['Nexus'].room.name); //se
-      terrain[6] = Game.map.getTerrainAt(nodes[source][1]-1,(nodes[source][2]+1),Game.spawns['Nexus'].room.name); //nw
-      terrain[7] = Game.map.getTerrainAt(nodes[source][1]-1,(nodes[source][2]-1),Game.spawns['Nexus'].room.name); //sw
 
-      for(var tile in terrain) {
-        if(terrain[tile]!="wall")
-        {
-            nodes[source][4]++;
-        }
-      }
-
-      //console.log(nodes[source]);
-  }
 
   for(var i in Memory.creeps) {
     if(!Game.creeps[i]) {
@@ -87,20 +101,14 @@ module.exports.loop = function () {
 
     for(var i in nodes)
     {
-        console.log(_.size(nodes[i][3]) + "|" + nodes[i][4]);
         if(_.size(nodes[i][3]) < nodes[i][4])
         {
             creationassignednode = nodes[i][0];
-            nodes[i][3].push(name);
             break;
         }
     }
 
-    console.log("Creation assigned node:" + creationassignednode);
-
-
-
-    Game.spawns['Nexus'].spawnCreep( [WORK, CARRY, MOVE, MOVE] , name  , {memory: {role: 'harvester', assignednode: creationassignednode}});
+    Game.spawns['Nexus'].spawnCreep( [WORK, CARRY, MOVE, MOVE] , name  , {memory: {role: 'harvester', assignednode: creationassignednode}})
 
 
   }
@@ -123,16 +131,9 @@ module.exports.loop = function () {
         }
     }
 
-
-
-
       Game.spawns['Nexus'].spawnCreep( [WORK, CARRY, MOVE, MOVE] , name  , {memory: {role: 'enhancer'}});
     }
   }
-
-
-
-
 
   for(var name in Game.creeps) {
         var creep = Game.creeps[name];
@@ -143,6 +144,7 @@ module.exports.loop = function () {
             role_Enhancer.run(creep);
         }
     }
+
 
     console.log(nodes);
 
--- role_Harvester.js
+++ role_Harvester.js
@@ -1,16 +1,51 @@
 module.exports = {
   run: function(creep, nodes) {
-      
+
+        var safe = true;
+        var checksource = [];
+
+        var sources = Game.getObjectById(creep.memory.assignednode["id"]);
+
+        //see if there are any enemies near the source
+
+        checksource[0] = (creep.memory.assignednode["pos"]["y"] + 10);//top
+        checksource[1] = (creep.memory.assignednode["pos"]["y"] - 10);//bottom
+        checksource[2] = (creep.memory.assignednode["pos"]["x"] + 10);//left
+        checksource[3] = (creep.memory.assignednode["pos"]["x"] - 10);//right
+
+        console.log("T:" + checksource[0] + ", B:" + checksource[1] + " ,L:" + checksource[2] + " ,R:" + checksource[3] )
+        console.log(creep.room.lookForAtArea(LOOK_CREEPS, checksource[0], checksource[2], checksource[1], checksource[3]))
+        /*
+        var creepsInArea = creep.room.lookForAtArea(LOOK_CREEPS, checksource[0], checksource[2], checksource[1], checksource[3], true);
+        var find_Enemy = false;
+        for(creeps in creepsInArea)
+        {
+            var currentcreep = creepsInArea[creep]
+            if currentcreep.my == "false"
+            {
+                find_Enemy = true;
+            }
+        }
+
+        //if there is, try reassign the source
+
+        if(find_Enemy == true)
+        {
+
+        }
+
+        //else sit next to spawner until safe.
+
+         */
         if(creep.carry.energy < creep.carryCapacity) {
-            //var sources = creep.room.find(FIND_SOURCES);
-            var sources = Game.getObjectById(creep.memory.assignednode["id"]);
-            console.log(sources);
-            if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
+            if(creep.harvest(sources) == ERR_NOT_IN_RANGE)
+            {
                 creep.moveTo(sources);
             }
         }
         else {
-            if(creep.transfer(Game.spawns['Nexus'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            if(creep.transfer(Game.spawns['Nexus'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+            {
                 creep.moveTo(Game.spawns['Nexus']);
             }
         }
